3d78df7ed0d242508f5015d7381636a5f5a629ea
Nick Neisen <nwneisen@gmail.com>
2024-08-12 12:01:39 -0600

    Run the project

--- cmd/dex-http-server/main.go
+++ cmd/dex-http-server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
+	"nwneisen/dex-http-server/gen/go/api"
 
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/grpclog"
 )
 
@@ -17,20 +22,22 @@ var (
 )
 
 func run() error {
-	// ctx := context.Background()
-	// ctx, cancel := context.WithCancel(ctx)
-	// defer cancel()
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
-	// opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	// err := gw.RegisterYourServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
-	// if err != nil {
-	// 	return err
-	// }
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := api.RegisterDexHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	if err != nil {
+		return err
+	}
+	log.Info().Msgf("Registered gRPC server endpoint: %s", *grpcServerEndpoint)
 
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	log.Info().Msgf("Starting HTTP server on %s", port)
 	return http.ListenAndServe(":"+*port, mux)
 }
 
