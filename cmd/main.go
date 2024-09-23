a152e6c3641e30087ff919a122d42ca8075aca42
Ranyodh Singh <ranyodh@gmail.com>
2024-09-23 11:22:01 -0400

    [BOP-711] Read gRPC certs from the provided path in cli arg (#3)
    * Read gRPC certs from the provided path from cli args

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/grpclog"
 
 	"github.com/mirantiscontainers/dex-http-server/gen/go/api"
+	"github.com/mirantiscontainers/dex-http-server/internal/tls"
 )
 
 var (
@@ -22,6 +25,9 @@ var (
 	// HTTP server port
 	port = flag.String("http-port", "8080", "HTTP server port")
 
+	// HTTP server port
+	certsPath = flag.String("grpc-certs-path", "", "Path to the directory containing the grpc certs")
+
 	version, commit, date = "", "", "" // These are always injected at build time
 )
 
@@ -30,12 +36,25 @@ func run() error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	var err error
+	var creds credentials.TransportCredentials
+
+	// Load the cert from the file, if provided
+	if *certsPath != "" {
+		log.Info().Msgf("Using cert for grpc connect from %s", *certsPath)
+		creds, err = getDexGrpcCredentials(*certsPath)
+		if err != nil {
+			return fmt.Errorf("failed to get grpc credentials: %w", err)
+		}
+	} else {
+		log.Info().Msg("No cert provided, using insecure connection")
+		creds = insecure.NewCredentials()
+	}
+
 	// Register gRPC server endpoint
-	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err := api.RegisterDexHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
-	if err != nil {
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+	if err = api.RegisterDexHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts); err != nil {
 		return err
 	}
 	log.Info().Msgf("Registered gRPC server endpoint: %s", *grpcServerEndpoint)
@@ -43,6 +62,15 @@ func run() error {
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
 	log.Info().Msgf("Running HTTP server on %s", *port)
 	return http.ListenAndServe(":"+*port, mux)
+}
+
+func getDexGrpcCredentials(tlsDir string) (credentials.TransportCredentials, error) {
+	tlsConfig, err := tls.LoadTLSConfig(tlsDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load TLS config: %w", err)
+	}
+	return credentials.NewTLS(tlsConfig), nil
+
 }
 
 func main() {
---
+++ internal/tls/tls.go
@@ -0,0 +1,60 @@
+package tls
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"os"
+	"path"
+
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	caCertFile = "ca.crt"
+	certFile   = "tls.crt"
+	keyFile    = "tls.key"
+)
+
+// LoadTLSConfig loads the TLS configuration from the provided directory
+func LoadTLSConfig(certDir string) (*tls.Config, error) {
+	log.Info().Msgf("Loading TLS configuration from %s", certDir)
+	ca, err := readFile(path.Join(certDir, caCertFile))
+	cert, err := readFile(path.Join(certDir, certFile))
+	key, err := readFile(path.Join(certDir, keyFile))
+
+	cPool := x509.NewCertPool()
+	if !cPool.AppendCertsFromPEM(ca) {
+		return nil, fmt.Errorf("unable to parse CA crt from file %s", path.Join(certDir, caCertFile))
+	}
+
+	clientCert, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return nil, fmt.Errorf("invalid client crt data from file %s: %v", path.Join(certDir, certFile), err)
+	}
+
+	clientTLSConfig := &tls.Config{
+		RootCAs:      cPool,
+		Certificates: []tls.Certificate{clientCert},
+	}
+
+	return clientTLSConfig, nil
+}
+
+func readFile(path string) ([]byte, error) {
+	log.Debug().Msgf("Reading file %s", path)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %s: %w", path, err)
+	}
+
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %s: %w", path, err)
+	}
+
+	return data, nil
+}
