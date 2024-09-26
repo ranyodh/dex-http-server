f6a06bbf6d00a5a57bbb2329ab05b702acbd1fca
Ranyodh Singh <ranyodh@gmail.com>
2024-09-26 13:33:44 -0400

    Remove --disable-auth arg (#9)

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -7,21 +7,22 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// Middleware is a function that wraps an http.HandlerFunc
-//type Middleware func(http.HandlerFunc) http.HandlerFunc
-
 // GetMiddlewares returns the list of middlewares to be applied to the request
-func GetMiddlewares(authDisabled bool) []runtime.Middleware {
+func GetMiddlewares() []runtime.Middleware {
+	// List of middlewares
+	// Order of middlewares is important
+	// Middlewares are applied in the order they are added in the list
 	mws := []runtime.Middleware{
 		loggingMiddleware,
-	}
 
-	// If authentication is not disabled, add the authentication middleware
-	if !authDisabled {
-		mws = append(mws, authMiddleware()...)
-	}
+		// auth middlewares
+		authenticationMiddleware(),
+		authorizationMiddleware(),
 
-	mws = append(mws, userRequestsMiddleware()...)
+		// user create/update interceptor middlewares
+		createUserMiddleware,
+		updateUserMiddleware,
+	}
 	return mws
 
 }
@@ -31,23 +32,5 @@ func loggingMiddleware(next runtime.HandlerFunc) runtime.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
 		log.Info().Msgf("%s %s", r.Method, r.URL.Path)
 		next(w, r, pathParams)
-	}
-}
-
-// authMiddleware chains the authentication and authorization middleware functions
-func authMiddleware() []runtime.Middleware {
-	// The list of middleware functions to be applied to the request
-	// Note: The order of the middleware functions is important as it
-	//       defines the order in which they are applied
-	return []runtime.Middleware{
-		authenticationMiddleware(),
-		authorizationMiddleware(),
-	}
-}
-
-func userRequestsMiddleware() []runtime.Middleware {
-	return []runtime.Middleware{
-		createUserMiddleware,
-		updateUserMiddleware,
 	}
 }
